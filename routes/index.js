521ef9bdbf650115f9c08a0dab8e72e69e1b0a6f
Mark Austin <mark@markaustinonline.com>
2018-01-30 12:24:26 -0800

    Passport, authentication fixed; Moved to separate file

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+//user routes
+
+const 
+    express = require('express')
+    passport = require('passport')
+    userRouter = new express.Router()
+
+
+function isLoggedIn( req, res, next){
+    if(req.isAuthenticated()) return next()
+    res.redirect('/login')
+}
+
+// userRouter.get('/')
+
+// LOGIN -------- 
+userRouter.get('/login', (req, res) => {
+    res.render('login')
+})
+
+userRouter.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+}) )
+
+// SIGNUP ------- 
+userRouter.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+userRouter.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup'
+}) )
+
+// PROFILE ------- 
+userRouter.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile', {user: req.user})
+})
+
+// LOGOUT ------- 
+userRouter.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+
+
+
+module.exports = userRouter
+
