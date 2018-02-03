732d5e14b9bff74d10101dcda93b4170a3c0e04a
Nina Klemencic <nina4klemencic@gmail.com>
2018-02-02 16:20:02 -0800

    guru search working

--- routes/gurusAll.js
+++ routes/gurusAll.js
@@ -1,18 +1,32 @@
 const
   express = require('express'),
   gurusAllRouter = new express.Router(),
-  Guru = require('../models/Guru.js'),
   flash = require('connect-flash'),
+
   Activity = require('../models/Activity.js'),
+  Guru = require('../models/Guru.js'),
+  User = require('../models/User.js'),
   Studio = require('../models/Studio.js')
 
 // Get all gurus:
 gurusAllRouter.get('/', (req, res) => {
   Guru.find({}, (err, allDemGurus) => {
     if(err) return console.log(err)
-    res.render('guru-index', {gurus: allDemGurus})
+    res.render('guru-index', {gurus: allDemGurus, user: req.user})
   })
 })
+
+// POST/SEARCH all activities:
+gurusAllRouter.get('/search/:term', (req, res) => {
+  const regex1 = new RegExp(req.params.term,'i');
+  console.log('Search Term (server side)  :' , req.params.term)
+  Guru.find({$or: [{name: regex1}, {studio: regex1}]}, (err, results) => {
+    console.log('RESULTS   :', results  )
+    res.json(results)
+  })
+})
+
+
 
 // Create a guru:
 gurusAllRouter.post('/', (req,res) => {
